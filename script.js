9925d523e6a8cc054b660ed41906fb46085b450f
itscc8 <78510340+itscc8@users.noreply.github.com>
2024-02-09 14:23:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ const editableDiv = document.getElementById('editableDiv')
 
 const storedValue = localStorage.getItem('content')
 if (storedValue.length > 3) {
-    editableDiv.innerText = storedValue
+  editableDiv.innerText = storedValue
 } else {
-    editableDiv.innerText = '...'
+  editableDiv.innerText = '...'
 }
 
 editableDiv.addEventListener('input', () => {
@@ -14,13 +14,14 @@ editableDiv.addEventListener('input', () => {
 editableDiv.addEventListener('keydown', (event) => {
   if (event.key === 'Enter') {
     const lines = editableDiv.innerText.split('\n')
-    const lastLine = lines.pop()
+    const lastLine = lines[lines.length - 2]
 
     if (lastLine.startsWith('@')) {
       try {
         const code = lastLine.substring(2)
         const result = eval(code)
-        editableDiv.innerText = lines.join('\n') + '\n' + 'RES: ' + result + '\n'
+        editableDiv.innerText =
+          lines.join('\n') + '\n' + 'RES: ' + result + '\n'
       } catch (error) {
         console.error('Evaluation error:', error)
         editableDiv.innerText =
